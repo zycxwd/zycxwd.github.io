61dcbf8874021095ee4075ce39353cafdb024c19
zycxwd <zycxwd@qq.com>
2017-04-27 10:42:32 +0800

    app1.1

--- app/js/main4.js
+++ app/js/main4.js
@@ -20,6 +20,9 @@ var Home = Vue.extend({
         }
     }
 });
+const xiangqing = {
+    template: '<div><p>列表详情展示</p>你现在的详情页为 {{ $route.params.author}}</div>'
+}
 var Seek = Vue.extend({
     data() {
         return {
@@ -31,14 +34,42 @@ var Seek = Vue.extend({
             sdActivel1: true,
             sdActiver1: false,
             dui1: false,
-            abc: false
-
+            abc: false,
+            citynameone: "北京",
+            citynametwo: "武汉"
         }
+    },
+    props: {
+        // city1:"河南",
+        // city1:"河动",
     },
     template: "#seek",
     methods: {
         grabble: function () {
-            this.$router.replace('/list1/flightList');
+            // 响应成功回调
+            this.$http.post("http://192.168.3.109:8081/server/PtOrderTFlightFindListA", {
+                    "departDate": "2017-4-28",
+                    "departAirport": "PEK",
+                    "empRank": "1",
+                    "tripType": "1",
+                    "arriveAirport": "WUH",
+                    "corpId": "ZJKYQY"
+                })
+                /*this.$http.get("js/list.json")*/
+                .then(
+                    function (res) {
+                        // 处理成功的结果
+                        this.$router.replace('/list1/flightList?cityone=' + this.citynameone + '&citytwo=' + this.citynametwo);
+                        console.log(res);
+                    },
+                    function (res) {
+                        // 处理失败的结果
+                        alert(res.status);
+                    }
+                );
+
+            // 响应错误回调
+
         },
         ticInquires: function () {
             this.$router.replace('/home/homepage');
@@ -142,14 +173,83 @@ var List1 = Vue.extend({
 });
 var Indent = Vue.extend({
     template: "#indent",
+    data() {
+        return {
+            jpmsgs: [{
+                    jpname: "张三",
+                    identity: "xxx20300xxx43",
+                    number: "13810730043"
+                },
+                {
+                    jpname: "李四",
+                    identity: "xxx20600xxx56",
+                    number: "15610730049"
+                }
+            ]
+        }
+    },
     methods: {
         fun: function () {
             console.log(1)
         },
-        prev:function(){
-            
-        }
+        prev: function () {
+            this.$router.replace('/list1/flightList');
+        },
+        jpsubmit: function () {
+            this.$http.post("http://192.168.3.109:8081/server/PtOrderTPtOrderSaveA", {
+                "sendMode": "配送方式",
+                "rangeList": [{
+                    "cabin": "舱位",
+                    "cabinName": "舱位名称",
+                    "model": "机型",
+                    "arriveTerminal": "到达航站楼",
+                    "departAirportName": "首都国际机场",
+                    "flightNo": "航班号",
+                    "maxStandardPrice": 1.00,
+                    "departAirport": "PEK",
+                    "saleDiscount": 9,
+                    "arriveCity": "武汉",
+                    "arriveCityName": "到达城市名称",
+                    "departTermina": "起飞航站楼",
+                    "flightTime": 24,
+                    "sn": "12",
+                    "yPrice": 22.00,
+                    "departTime": "2017-04-28",
+                    "departCity": "北京",
+                    "arriveAirportName": "天河机场",
+                    "arriveTime": "到达时间",
+                    "standardPrice": 22.00,
+                    "minPriceV": 22.00,
+                    "departCityName": "出发城市名称",
+                    "arriveAirport": "WUH",
+                    "minPrice": 22.00
+                }],
+                "supplierId": "111",
+                "linkTel": "13810730042",
+                "linkMobile": "13810730042",
+                "tripType": "1",
+                "linkMan": "张",
+                "psgList": [{
+                    "idType": "身份证",
+                    "sn": 22,
+                    "psgType": "1",
+                    "phoneNumber": "13810730043",
+                    "idno": "110222199010484587",
+                    "psgName": this.jpmsgs[0].jpname
+                }]
+            }).then(function (res) {
+                // 处理成功的结果
+                console.log(res)
+                alert("请求成功订单编号为：" + res.bodyText.content)
+                console.log(res.bodyText)
 
+                // alert("订票信息已经发出")
+                // console.log(this.jpmsgs[0].jpname)
+            }, function (res) {
+                // 处理失败的结果
+                alert(res);
+            });
+        }
     }
 })
 //机票详情组件
@@ -184,7 +284,7 @@ router.map({
                             this.$router.replace('/seek');
                         },
                         scli: function () {
-                            alert(1)
+                            // alert(1)
                         },
                         dcli: function () {
                             console.log(this.$router)
@@ -227,77 +327,101 @@ router.map({
                     template: '#flightList',
                     data() {
                         return {
-                            todos: [{
-                                    timem: "00:10",
-                                    timee: "03:10",
-                                    text1: '北京-河北',
-                                    text2: '',
-                                    text3: '¥113',
-                                },
-                                {
-                                    timem: "09:30",
-                                    timee: "13:10",
-                                    text1: '北京-河北',
-                                    text2: '',
-                                    text3: '¥613',
-                                },
-                                {
-                                    timem: "06:10",
-                                    timee: "18:10",
-                                    text1: '北京-河北',
-                                    text2: '违',
-                                    text3: '¥913',
-                                },
-                                {
-                                    timem: "08:40",
-                                    timee: "12:10",
-                                    text1: '北京-河北',
-                                    text2: '',
-                                    text3: '¥313',
-                                },
-                                {
-                                    timem: "00:10",
-                                    timee: "06:10",
-                                    text1: '北京-河北',
-                                    text2: '',
-                                    text3: '¥6113',
-                                },
-                                {
-                                    timem: "10:10",
-                                    timee: "16:20",
-                                    text1: '北京-河北',
-                                    text2: '',
-                                    text3: '¥513',
-                                }
-                            ]
+                            todos: []
                         }
+                    },
+                    creaded: function () {
+                        this.urlfunction();
+                    },
+                    compiled: function () {
+                        //在编译结束后调用。此时所有的指令已生效，因而数据的变化将触发 DOM 更新。但是不担保 $el 已插入文档。
+                        this.urlfunction();
                     },
                     methods: {
 
                         clic: function ($index) {
-                            for (var index = 0; index < this.todos.length; index++) {
-                                console.log(typeof this.todos[index].text3.slice(1))
 
-                            }
 
-                            this.$router.go('/list1/serviceProvider');
+                        },
+                        urlfunction: function () {
+                            console.log(this.$route.query.citytwo)
+                            this.$http.post("http://192.168.3.109:8081/server/PtOrderTFlightFindListA", {
+                                "departDate": "2017-4-28",
+                                "departAirport": "PEK",
+                                "empRank": "1",
+                                "tripType": "1",
+                                "arriveAirport": "WUH",
+                                "corpId": "ZJKYQY"
+                            }).then(
+                                function (res) {
+                                    // 处理成功的结果
+                                    console.log(res);
+                                    this.todos = res.data.content.flightList
+
+                                    console.log(this.todos);
+                                    if (window.localStorage) {
+                                        window.localStorage.setItem("localPrizeData", JSON.stringify(this.todos));
+                                    }
+                                },
+                                function (res) {
+                                    // 处理失败的结果
+                                    // alert(1);
+                                })
                         }
                     }
                 }
+                /*,
+                                subRoutes: {
+                		            '/:flightNo': {
+                		                component: {
+                		                    data() {
+                		                        return {
+                		                            msg: "111"
+                		                        }
+                		                    },
+                		                    template: '#serviceProvider',
+                		                    methods: {
+                		                        indent: function () {
+                		                            this.$router.replace('/indent');
+                		                        }
+                		                    }
+                		                }
+                		            }
+                		       }*/
             },
-            'serviceProvider': {
+            '/:flightNo': {
                 component: {
                     data() {
                         return {
-                            msg: "111"
+                            tamas: []
                         }
+                    },
+                    compiled: function () {
+                        //在编译结束后调用。此时所有的指令已生效，因而数据的变化将触发 DOM 更新。但是不担保 $el 已插入文档。
+                        this.cokie();
                     },
                     template: '#serviceProvider',
                     methods: {
                         indent: function () {
                             this.$router.replace('/indent');
+                        },
+                        cokie: function () {
+                            // alert()
+                            if (window.localStorage) {
+                                this.qtodos = JSON.parse(window.localStorage.getItem("localPrizeData"));
+                            }
+                            console.log(this.qtodos);
+
+                            for (var i = 0; i < this.qtodos.length; i++) {
+                                if (this.qtodos[i].flightNo == this.$route.params.flightNo) {
+                                    this.tamas = this.qtodos[i]
+                                }
+                            }
+                            console.log(this.tamas);
+                            console.log(this.$route.params.flightNo);
                         }
                     }
+
                 }
             },
             'cost': {
@@ -310,7 +434,7 @@ router.map({
                     template: '#cost',
                     methods: {
                         fun: function () {
-                            alert(1)
+                            // alert(1)
                         }
                     }
                 }
@@ -324,11 +448,13 @@ router.map({
                     },
                     template: '#filtrate'
                 }
+
             }
         }
     },
     'indent': {
         component: Indent
+
     },
     'listsub': {
         component: Listsub
